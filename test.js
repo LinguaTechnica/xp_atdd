a98ac8f6f61a5b1cb8881bbd8922e370baec1209
K Hudson <kay.hudson@galvanize.com>
2020-10-23 14:34:19 -0700

    Update test language and fix failing test.

--- test.js
+++ test.js
@@ -1,25 +1,35 @@
 const { GetQuotePage } = require('./get_quote');
 
+const Given = (desc, callback) => {
+    return describe(desc, callback)
+}
+const When = (desc, callback) => {
+    return describe(desc, callback)
+}
+const Then = (desc, callback) => {
+    return it(desc, callback)
+}
+
 describe('Get Quote Feature', () => {
     let getQuotePage;
 
-    describe('Given I am at the get quote page',()=> {
+    Given('Given I am at the get quote page',()=> {
         beforeEach(() => {
             getQuotePage = new GetQuotePage();
         })
-        describe('When I fill out my home address and service type', () => {
-            it('Then I can see a price quote for the service I chose',  () => {
+        When('When I fill out my home address and service type', () => {
+            Then('Then I can see a price quote for the service I chose',  () => {
                 getQuotePage.serviceType = 'Home Insurance';
                 getQuotePage.address = '123 Main Street';
                 expect(getQuotePage.isValid()).toBe(true);
             });
         });
 
-        describe('When I leave my home address incomplete',  () => {
-            it('Then I see a message to provide it',  () => {
+        When('When I select a service and leave my home address incomplete',  () => {
+            Then('Then I see a message to provide my home address',  () => {
                 getQuotePage.serviceType = 'Home Insurance';
                 expect(getQuotePage.isValid()).toBe(false);
-                expect(getQuotePage.getResponse()).toBe('Please select a service.')
+                expect(getQuotePage.getResponse()).toEqual('Please provide your address.')
             });
         });
 
