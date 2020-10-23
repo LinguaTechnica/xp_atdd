2fe488119bda98981bc29b5209f54c5aca24a979
K Hudson <kay.hudson@galvanize.com>
2020-10-23 09:27:22 -0700

    Fix syntax.

--- test.js
+++ test.js
@@ -7,16 +7,16 @@ describe('Get Quote Feature', () => {
         beforeEach(() => {
             getQuotePage = new GetQuotePage();
         })
-        describe('When I fill out my home address and service type', function () {
-            it('Then I can see a price quote for the service I chose', function () {
+        describe('When I fill out my home address and service type', () => {
+            it('Then I can see a price quote for the service I chose',  () => {
                 getQuotePage.serviceType = 'Home Insurance';
                 getQuotePage.address = '123 Main Street';
                 expect(getQuotePage.isValid()).toBe(true);
             });
         });
 
-        describe('When I leave my home address incomplete', function () {
-            it('Then I see a message to provide it', function () {
+        describe('When I leave my home address incomplete',  () => {
+            it('Then I see a message to provide it',  () => {
                 getQuotePage.serviceType = 'Home Insurance';
                 expect(getQuotePage.isValid()).toBe(false);
                 expect(getQuotePage.getResponse()).toBe('Please select a service.')
